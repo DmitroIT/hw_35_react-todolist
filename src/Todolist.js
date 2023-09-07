4507538ecc205f06431295d0b61a83d9f06db67a
Dmytro Loshyk <dmitroloshik@gmail.com>
2023-09-07 16:42:18 +0300

    correction of comments

--- src/Todolist.js
+++ src/Todolist.js
@@ -2,52 +2,56 @@ import React, { useState, useCallback } from "react";
 import "./Todolist.css"
 
 function TodoList() {
-
     const todolist = ["todo_1", "todo_2", "todo_3"];
 
-    const [todos, setTodos] = useState(todolist)
+    const todolistMap = todolist.map((todo) => ({ text: todo, completed: false }))
+
+    const [todos, setTodos] = useState(todolistMap);
     const [newTodo, setNewTodo] = useState("");
 
     const handleAddTodo = useCallback(() => {
         if (newTodo.trim() !== "") {
-            setTodos((prevTodos) => [...prevTodos, newTodo]);
+            setTodos((prevTodos) => [
+                ...prevTodos,
+                { text: newTodo, completed: false },
+            ]);
             setNewTodo("");
         }
     }, [newTodo]);
 
-    const handleDeleteTodo = useCallback((index) => {
-        setTodos((prevTodos) => prevTodos.filter((_, i) => i !== index));
+    const toggleTodoCompletion = useCallback((index) => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo, i) =>
+                i === index
+                    ? { ...todo, completed: !todo.completed }
+                    : todo
+            )
+        );
     }, []);
 
-    const handleComplited = useCallback(() => {
-
-        const todoItem = document.querySelectorAll(".todo__item")
-        todoItem.forEach((todo) => {
-            todo.classList.toggle("todo__item--complited")
-        })
-    })
-
-        return(
-            <div>
-                <h1>создай todo list</h1>
-                <ol>
-                    {todos.map((todo, index) => (
-                        <li className="todo__item" key={index}>
-                            {todo}
-                            <button onClick={() => handleDeleteTodo(index)}>удалить</button>
-                            <button onClick={() => handleComplited(index)}>выполнено</button>
-                        </li>
-                    ))}
-                </ol>
-                <input
-                    type="text"
-                    value={newTodo}
-                    onChange={(e) => setNewTodo(e.target.value)}
-                    placeholder="enter your todo"
-                />
-                <button onClick={handleAddTodo}>добавить</button>
-
-            </div>
-        );
+    return (
+        <div>
+            <h1>Создай todo list</h1>
+            <ol>
+                {todos.map((todo, index) => (
+                    <li
+                        className={`todo__item ${todo.completed ? "completed" : ""}`}
+                        key={index}
+                        onClick={() => toggleTodoCompletion(index)}
+                    >
+                        {todo.text}
+                    </li>
+                ))}
+            </ol>
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="Enter your todo"
+            />
+            <button onClick={handleAddTodo}>Добавить</button>
+        </div>
+    );
 }
+
 export default TodoList;
